9ad43162a54521ab975c4ebd3ab9cb3cb3e8c999
takaomiyamashita <ty.8479@gmail.com>
2020-10-31 10:54:36 +0900

    (仮)ボタンごとに異なった結果になる
    複数のsubmitボタンをつくり、それぞれ異なる結果になるように実装した

--- confirmation.php
+++ confirmation.php
@@ -1,7 +1,11 @@
 <?php
-$exe=$_POST['execution'];
 
-if($exe==true){
-  print 'ポイントが貯まったよ！';
-}
+if(isset($_POST['execution1'])){
+  echo "お皿洗いしてくれてありがとう！";
+}elseif(isset($_POST['execution2'])){
+  echo "お風呂掃除してくれてありがとう！";
+}elseif(isset($_POST['execution3'])){
+  echo "掃除機がけしてくれてありがとう！";
+};
+
 ?>
--- index.php
+++ index.php
@@ -6,7 +6,12 @@
 </head>
 <body>
 <form method="post" action="confirmation.php">
-<input type="submit" name="execution" value="やったよ！">
+<div class="list" name="list1">皿洗い<div>
+<input type="submit" name="execution1" value="やったよ！"><br><br>
+<div class="list" name="list2">お風呂掃除<div>
+<input type="submit" name="execution2" value="やったよ！"><br><br>
+<div class="list" name="list3">掃除機がけ<div>
+<input type="submit" name="execution3" value="やったよ！"><br><br>
 </form>
 </body>
 </html>
