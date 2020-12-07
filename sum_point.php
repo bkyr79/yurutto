4275d9a9491b56048ca49aae88af2b2361d9c362
takaomiyamashita <ty.8479@gmail.com>
2020-12-08 08:26:35 +0900

    GETしたご褒美の表示
    必要ポイントに達したら、ご褒美を表示される

--- sum_point.php
+++ sum_point.php
@@ -1,13 +1,12 @@
 <?php
-try
-{
 $dsn = 'mysql:dbname=yurutto;host=localhost;charset=utf8';
 $user='root';
 $password='';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql = 'SELECT SUM(point) as sum_point FROM users';
+$sql = 'SELECT SUM(point) as sum_point FROM users;';
+
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 
@@ -16,12 +15,33 @@ $sum_point=$rec['sum_point'];
 
 $dbh = null;
 
-print "合計ポイントは".$sum_point."P! ";
+$con = mysqli_connect('127.0.0.1', 'root', '');
+if (!$con) {
+  exit('データベースに接続できませんでした。');
 }
-catch (Exception $e)
-{
+$result = mysqli_select_db($con, 'yurutto');
+if (!$result) {
+  exit('データベースを選択できませんでした。');
+}
+$result = mysqli_query($con, 'SET NAMES utf8');
+if (!$result) {
+  exit('文字コードを指定できませんでした。');
+}
+$result = mysqli_query($con, 'SELECT * FROM gohobi ORDER BY necessary_point DESC');
+while ($data = mysqli_fetch_array($result)) {
+  for ($i=$sum_point; $sum_point>=$data['necessary_point']; $i--){
+  // if($sum_point>=$data['necessary_point']){
+    print "合計ポイントは".$sum_point."P! ";
+    print "ご褒美『".$data['gohobi_name']."』をGETだ!";
+    return false;
+    // if($data>1){
+    //   print "文章2つだねー";
+    // }
+  }
+}
 
-  print 'ただいま障害により大変ご迷惑をお掛けしております。';
-  exit();
+$con = mysqli_close($con);
+if (!$con) {
+  exit('データベースとの接続を閉じられませんでした。');
 }
 ?>
